6387b8eaf4c794a82de4779b2fc564bca40ef385
joknarf <joknarf@free.fr>
2024-12-12 21:32:51 +0100

    media searchtxt icon

--- pywebfs/pywebfs.py
+++ pywebfs/pywebfs.py
@@ -224,7 +224,7 @@ CSS = f"""
         body {{
             -webkit-text-size-adjust: 180%;
         }}
-        .search, .home, .folder, .file, .link, .upfolder {{
+        .search, .searchtxt, .home, .folder, .file, .link, .upfolder {{
             background-size: 32px 32px;
             text-indent: 40px;
         }}
